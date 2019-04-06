d38dddf2a9ee7f66a6f4aee9a134acdf2a64ca7c
Dod-Annie <936166900@qq.com>
2019-04-06 14:03:21 +0800

    add homepage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import Login from './containers/login'
+import HomePage from './containers/homepage'
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Login/>
-      </div>
+      <Router>
+        <div>
+          <Route path="/" exact component={Login} />
+          <Route path="/login/" component={Login} />
+          <Route path="/index/" component={HomePage} />
+        </div>
+      </Router>
     );
   }
 }
